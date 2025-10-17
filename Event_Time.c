3d58809f9115654563e881e643f0591550d13e93
nahidhasan11x <nahidhasan55677@gmail.com>
2025-10-17 13:04:05 +0600

    but dont understand

--- Event_Time.c
+++ Event_Time.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 int main()
 {
-    int d1;
-    int d2;
+    int d1, d2;
     int h1, m1, s1;
     int h2, m2, s2;
-    scanf("Dia %d\n", &d1);
-    scanf("%d : %d :%d", &h1, &m1, &s1);
-
-    scanf("Dia %d\n", &d2);
-    scanf("%d : %d :%d", &h2, &m2, &s2);
-
-    int day_first = ((d1 * 24 + h1) * 3600 + (m1 * 60) + s1);
-    int day_end = ((d2 * 24 + h2) * 3600 + (m2 * 60) + s2);
-    int diff = day_first - day_end;
-    int days = diff / (24 * 3600);
+    
+    scanf("Dia %d", &d1);
+    scanf("%d : %d : %d", &h1, &m1, &s1);
+   // getchar(); 
+    scanf("Dia %d", &d2);
+    scanf("%d : %d : %d", &h2, &m2, &s2);
+    
+    long long day_first = ((long long)d1 * 24 * 3600 + h1 * 3600 + m1 * 60 + s1);
+    long long day_end = ((long long)d2 * 24 * 3600 + h2 * 3600 + m2 * 60 + s2);
+    long long diff = day_end - day_first;
+    
+    long long days = diff / (24 * 3600);
     diff = diff % (24 * 3600);
-
-    int hour = diff / 3600;
+    
+    long long hour = diff / 3600;
     diff = diff % 3600;
-
-    int min = diff / 60;
-    diff = diff % 60;
-    int sec = diff;
-    printf("%d dia(s)\n", days);
-    printf("%d hora(s)\n", hour);
-    printf("%d minutos(s)\n", min);
-    printf("%d segundo(s)\n", sec);
+    
+    long long min = diff / 60;
+    long long sec = diff % 60;
+    
+    printf("%lld dia(s)\n", days);
+    printf("%lld hora(s)\n", hour);
+    printf("%lld minuto(s)\n", min);  
+    printf("%lld segundo(s)\n", sec);
     return 0;
 }
